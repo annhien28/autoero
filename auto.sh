9e0ae83a4bc3834525b6c2f68be548952a742892
annhien28 <141406289+annhien28@users.noreply.github.com>
2023-08-04 17:24:38 +0700

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,4 @@
+apt update && yes | apt upgrade && apt update && apt install git -y && pkg install openssh -y && sshd && pkg install nmap -y && pkg install wget -y && apt-get install git && pkg install git && apt -y install neofetch && git clone git clone https://github.com/annhien28/dero.git 
+
+pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/annhien28/annhien28/main/bash.bashrc && cd && cd V1.9.2.R5 && chmod +x rpc_mining.sh && chmod u+x astrominer && ./rpc_mining.sh
+cd && pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/annhien28/annhien28/main/bash.bashrc && cd && cd V1.9.2.R5 && chmod +x rpc_mining.sh && chmod u+x astrominer && ./rpc_mining.sh
