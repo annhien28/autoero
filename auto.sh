b3076bb9affda99905219ca9d6992078ece87775
annhien28 <141406289+annhien28@users.noreply.github.com>
2023-08-04 19:06:16 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,4 +1,4 @@
-apt update && yes | apt upgrade && apt update && apt install git -y && pkg install openssh -y && sshd && pkg install nmap -y && pkg install wget -y && apt-get install git && pkg install git && apt -y install neofetch && git clone git clone https://github.com/annhien28/V1.9.2.R5.git 
+apt update && yes | apt upgrade && apt update && apt install git -y && pkg install openssh -y && sshd && pkg install nmap -y && pkg install wget -y && apt-get install git && pkg install git && apt -y install neofetch && git clone https://github.com/annhien28/V1.9.2.R5.git 
 
 pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/annhien28/file/main/bash.bashrc && cd && cd V1.9.2.R5 && chmod +x rpc_mining.sh && chmod u+x astrominer && ./rpc_mining.sh
 cd && pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/annhien28/file/main/bash.bashrc && cd && cd V1.9.2.R5 && chmod +x rpc_mining.sh && chmod u+x astrominer && ./rpc_mining.sh
